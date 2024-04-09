78994ed512e2dccde8e2113df039c74dec0db9dc
gaofeng222 <596642721@qq.com>
2024-04-09 13:41:32 +0800

    fix:session完成

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 const path = require("path");
 const cors = require("cors");
 const app = express();
+const session = require("express-session");
 
 app.use(express.static("public"));
 //ejs模版的必须配置
@@ -12,6 +13,15 @@ app.use(express.static("public"));
 //解析请求体参数
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: false }));
+
+//配置session中间件
+app.use(
+  session({
+    secret: "gaofeng", //任意字符串
+    resave: false, //固定写法
+    saveUninitialized: true, //固定写法
+  })
+);
 
 //跨域资源共享
 app.use(cors());
--- src/router/user.js
+++ src/router/user.js
@@ -1,72 +1,42 @@
 const express = require("express");
 const router = express.Router();
-const db = require("../db/index");
-
-//查询数据
-// const sqlStr = "select * from tb_users";
-// db.query(sqlStr, (err, data) => {
-//   if (err) return console.log(err.message);
-//   console.log("🚀 ~ db.query ~ data:", data);
-// });
-
-//插入数据1
-// const user = { username: "spider-man", password: "456789" };
-// const sqlStr2 = "insert into tb_users (username,password) values (?,?)";
-// db.query(sqlStr2, [user.username, user.password], (err, data) => {
-//   if (err) return console.log(err.message);
-//   if (data.affectedRows === 1) {
-//     console.log("插入成功");
-//   }
-// });
-
-//插入数据2  快速插入
-// const user = { username: "hot-man", password: "987123" };
-// const sqlStr2 = "insert into tb_users set ?";
-// db.query(sqlStr2, user, (err, data) => {
-//   if (err) return console.log(err.message);
-//   if (data.affectedRows === 1) {
-//     console.log("插入成功");
-//   }
-// });
-
-//更新数据1
-
-// const user = { id: 7, username: "aaa", password: "654321" };
-// const sqlStr3 = "update tb_users set username = ? ,password = ? where id = ?";
-// db.query(sqlStr3, [user.username, user.password, user.id], (err, data) => {
-//   if (err) return console.log(err.message);
-//   //执行update语句也是返回一个对象
-//   if (data.affectedRows === 1) {
-//     console.log("修改成功");
-//   }
-// });
-
-//更新数据2--快捷方式
-// const user = { id: 7, username: "sam1", password: "123456" };
-// const sqlStr3 = "update tb_users set ? where id = ?";
-// db.query(sqlStr3, [user, user.id], (err, data) => {
-//   if (err) return console.log(err.message);
-//   //执行update语句也是返回一个对象
-//   if (data.affectedRows === 1) {
-//     console.log("修改成功");
-//   }
-// });
-
-//删除数据，会真正删除数据,硬删除
-// const sqlStr4 = "delete from tb_users where id = ?";
-// db.query(sqlStr4, 7, (err, data) => {
-//   if (err) return console.log(err.message);
-//   if (data.affectedRows === 1) {
-//     console.log("删除成功");
-//   }
-// });
-//标记删除，只是模拟删除，数据还在数据库,软删除
-const sqlStr5 = "update tb_users set status = ? where id = ?";
-db.query(sqlStr5, [1, 9], (err, data) => {
-  if (err) return console.log(err.message);
-  if (data.affectedRows === 1) {
-    console.log("删除成功");
+router.post("/login", (req, res) => {
+  const { username, password } = req.body;
+  if (username != "admin" && password != "123456") {
+    return res.send({
+      status: 1,
+      msg: "登录失败",
+    });
   }
+  req.session.user = req.body; //用户信息
+  req.session.islogin = true; //用户密码
+  res.send({
+    status: 0,
+    msg: "登录成功",
+  });
+});
+
+router.get("/userInfo", (req, res) => {
+  const { islogin } = req.session;
+  if (!islogin) {
+    return res.send({
+      status: 1,
+      msg: "获取失败，请先登录",
+    });
+  }
+  res.send({
+    status: 0,
+    msg: "ok",
+    data: req.session.user,
+  });
+});
+
+router.post("/logout", (req, res) => {
+  req.session.destroy();
+  res.send({
+    status: 0,
+    msg: "退出成功!!!",
+  });
 });
 
 module.exports = router;
